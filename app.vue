af2454e2eb3ff2ac8c9ccf273adfcfb8d19c4cee
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-11 09:30:03 +0200

    tabs

--- app.vue
+++ app.vue
@@ -1,6 +1,27 @@
 <template>
     <NuxtLayout>
         <v-app>
+            <v-app-bar density="comfortable">
+                <v-app-bar-title>
+                    MineSkin
+                    <v-btn icon>
+                        <v-icon>mdi-magnify</v-icon>
+                    </v-btn>
+                </v-app-bar-title>
+
+                <v-spacer></v-spacer>
+
+                <v-tabs
+                    v-model="tab"
+                    align-tabs="center"
+                >
+                    <v-tab value="one">Generate</v-tab>
+                    <v-tab value="two">Gallery</v-tab>
+                    <v-tab value="three">Item Three</v-tab>
+                </v-tabs>
+
+
+            </v-app-bar>
             <v-main>
                 <NuxtPage/>
             </v-main>
