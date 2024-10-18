ad0374f7fd3c3a608f2e6c42ae21710bdc18e415
Haylee Schäfer <haylee@inventivetalent.org>
2024-10-18 15:33:14 +0200

    add credit info

--- components/generate/GeneratePanel.vue
+++ components/generate/GeneratePanel.vue
@@ -146,11 +146,6 @@
         <v-expand-transition>
             <v-row v-show="generateType" class="my-4" justify="center">
                 <v-col>
-                    <v-row justify="center" class="mb-2 text-center" v-if="showCreditsInfo">
-                        <div>
-                            This request will consume 1 credit if the skin is successfully generated.
-                        </div>
-                    </v-row>
                     <v-row justify="center" class="mb-2 text-center">
                         <v-btn
                             color="primary"
@@ -161,7 +156,15 @@
                         ></v-btn>
                     </v-row>
                     <v-row justify="center" class="mt-2 text-center">
-                        <div v-show="generating">
+                        <div v-if="showCreditsInfo && !generating && (credits && credits.balance>0)">
+                            <span>This request will consume {{ imageCount || 1 }} {{ imageCount>1?'credits':'credit' }} if the skin is successfully generated.</span><br/>
+                            <span>You have {{ credits?.balance }} {{ credits?.type }} credits remaining.</span>
+                        </div>
+                        <div v-else-if="showCreditsInfo && !generating">
+                            <span>You do not have any credits remaining.</span><br/>
+                            <span>This skin may take longer to generate.</span>
+                        </div>
+                        <div v-if="generating">
                             <div>
                                 Your {{ imageCount > 1 ? 'skins are' : 'skin is' }} being generated...
                             </div>
@@ -190,7 +193,7 @@
 <script setup lang="ts">
 
 import { GenerateType, type Maybe, SkinVariant, SkinVisibility2 } from "@mineskin/types";
-import { useNuxtApp } from "nuxt/app";
+import { useLazyAsyncData, useNuxtApp } from "nuxt/app";
 import { useQueueStore } from "../../stores/queue";
 import { computed, ref } from "vue";
 import ActionLink from "../ActionLink.vue";
@@ -198,11 +201,21 @@ import GenerateUserSection from "./GenerateUserSection.vue";
 import GenerateUploadSection from "./GenerateUploadSection.vue";
 import GenerateUrlSection from "./GenerateUrlSection.vue";
 import type { GenerateJobResponse } from "~/types/GenerateJobResponse";
+import type { BasicCreditInfo } from "~/types/BasicCreditInfo";
+import { sleep } from "~/util/misc";
 
 const {$mineskin, $notify, $flags} = useNuxtApp();
 
 const authStore = useAuthStore();
 const queueStore = useQueueStore();
+
+const {
+    data: credits,
+    status: creditsStatus,
+} = useLazyAsyncData<BasicCreditInfo>("credits", async () => {
+    return (await $mineskin.me.credits())?.credit;
+});
+
 
 const generateType = computed<Maybe<GenerateType>>(() => {
     if (urls.value.filter(url => url.length > 0).length > 0) {
