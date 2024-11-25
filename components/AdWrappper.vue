31e53c73eb1d46f6f977bf7646787338421f485c
Haylee Schäfer <haylee@inventivetalent.org>
2024-11-25 14:25:45 +0100

    ClientOnly AdWrapper

--- components/AdWrappper.vue
+++ components/AdWrappper.vue
@@ -5,25 +5,27 @@
 }
 </style>
 <template>
-    <div class="ad-wrapper" v-if="!adFree">
-        <component is="script" async
-                   :src="'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=' + clientId"
-                   crossorigin="anonymous"></component>
-        <slot name="ad">
-            <ins class="adsbygoogle"
-                 style="display:block"
-                 :data-adtest="isDev ? 'on': ''"
-                 :data-ad-client="clientId"
-                 :data-ad-slot="adSlot"
-                 :data-ad-format="adFormat||'auto'"
-                 :data-ad-layout-key="adLayoutKey"
-                 :data-full-width-responsive="responsive||true"></ins>
-        </slot>
-        <component is="script">
-            {{ isDev ? 'google_adtest = "on";' : '' }}
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </component>
-    </div>
+    <ClientOnly>
+        <div class="ad-wrapper" v-if="!adFree">
+            <component is="script" async
+                       :src="'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=' + clientId"
+                       crossorigin="anonymous"></component>
+            <slot name="ad">
+                <ins class="adsbygoogle"
+                     style="display:block"
+                     :data-adtest="isDev ? 'on': ''"
+                     :data-ad-client="clientId"
+                     :data-ad-slot="adSlot"
+                     :data-ad-format="adFormat||'auto'"
+                     :data-ad-layout-key="adLayoutKey"
+                     :data-full-width-responsive="responsive||true"></ins>
+            </slot>
+            <component is="script">
+                {{ isDev ? 'google_adtest = "on";' : '' }}
+                (adsbygoogle = window.adsbygoogle || []).push({});
+            </component>
+        </div>
+    </ClientOnly>
 </template>
 <script setup lang="ts">
 import { useAuthStore } from "~/stores/auth";
