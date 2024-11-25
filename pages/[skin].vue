0419d6b82f20da109ebe6c7fa484c7f5bd8ced95
Haylee Schäfer <haylee@inventivetalent.org>
2024-11-25 16:33:31 +0100

    redirect

--- pages/[skin].vue
+++ pages/[skin].vue
@@ -1,77 +1,11 @@
 <style>
-.line-break-anywhere {
-    line-break: anywhere;
-}
 </style>
 <template>
     <v-container class="mb-4">
-        <h2>
-            <back-link to="/gallery" class="text-white">
-                <v-icon icon="mdi-arrow-left" class="mx-2"/>
-            </back-link>
-            {{ skinNameDisplay }}
-            <v-tooltip v-if="useRandomName" text="Random Name">
-                <template v-slot:activator="{ props }">
-                    <span v-bind="props">*</span>
-                </template>
-            </v-tooltip>
-            <v-tooltip
-                :text="skin?.visibility===SkinVisibility2.PRIVATE ? 'Private' : skin?.visibility===SkinVisibility2.UNLISTED? 'Unlisted':'Public'">
-                <template v-slot:activator="{ props }">
-                    <span v-bind="props">
-                        <v-icon class="mx-2">{{
-                                skin?.visibility === SkinVisibility2.PRIVATE ? 'mdi-eye-off' : skin?.visibility === SkinVisibility2.UNLISTED ? 'mdi-link' : 'mdi-earth'
-                            }}</v-icon>
-                    </span>
-                </template>
-            </v-tooltip>
-        </h2>
-        <v-row class="mt-1">
-            <v-col cols="12">
-                <SkinSummaryCard :skin="skin"/>
-            </v-col>
-        </v-row>
-        <v-row class="text-center">
-            <ad-wrappper ad-slot="4431802313"/>
-        </v-row>
-        <!--        <dbg :data="skin"/>-->
-        <v-row>
-            <v-col cols="12">
-                <SkinInstructionsCard :skin="skin"/>
-            </v-col>
-        </v-row>
-
-        <v-row class="my-4">
-            <v-col cols="12" md="6">
-                <div class="text-grey-darken-2">Generated on {{ new Date(skin?.generator?.timestamp).toUTCString() }} in
-                    {{ skin?.generator?.duration / 1000 }}s
-                </div>
-                <div class="text-grey-darken-3">By worker {{ skin?.generator?.worker || 'n/a' }} with account
-                    {{ skin?.generator?.account?.substring(0, 10) || 'n/a' }} via server
-                    {{ skin?.generator?.server || 'n/a' }}
-                </div>
-            </v-col>
-            <v-col cols="12" md="6" class="text-end line-break-anywhere">
-                <div class="text-grey-darken-3">{{ skin?.texture?.hash?.skin }}</div>
-            </v-col>
-        </v-row>
+        Moved!
     </v-container>
 </template>
 <script setup lang="ts">
-import { useLazyAsyncData, useNuxtApp } from "#app";
-import {
-    type Maybe,
-    type SkinIdAndTexture,
-    type SkinInfo,
-    type SkinInfo2,
-    SkinVisibility2,
-    type ValueAndSignature
-} from "@mineskin/types";
-import SkinSummaryCard from "~/components/skin/SkinSummaryCard.vue";
-import SkinInstructionsCard from "~/components/skin/SkinInstructionsCard.vue";
-import { skinName } from "../util/skin";
-import AdWrappper from "~/components/AdWrappper.vue";
-import { renderSkinBody, renderSkinHead } from "~/util/render";
 
 const router = useRouter();
 
@@ -80,71 +14,8 @@ const skinId = computed<string>(() => {
     return currentRoute.params['skin'] as string;
 });
 
-const {$mineskin} = useNuxtApp();
-
-const {
-    data: skin
-} = useLazyAsyncData<Maybe<SkinInfo2>>(`skin-${ skinId.value }`, async () => {
-    return (await $mineskin.skins.get(skinId.value))?.skin;
-});
-
-const {
-    data: randomSkinName
-} = useLazyAsyncData<string>(`skin-rng-name-${ skinId.value }`, async () => {
-    return (await $mineskin.util.randomName(skinId.value));
-});
-
-const useRandomName = computed(() => {
-    return !skin.value?.name && randomSkinName.value;
-})
-
-const skinNameDisplay = computed(() => {
-    if (useRandomName.value) {
-        return randomSkinName.value;
-    }
-    return skinName(skin.value, randomSkinName.value || 'Skin');
-});
-
-const skinHeadImage = computed(() => {
-    if (!skin.value) return null;
-    return renderSkinHead(skin.value.texture.hash.skin);
-});
-// const skinBodyImage = computed(() => {
-//     if (!skin.value) return null;
-//     return renderSkinBody(skin.value.texture.hash.skin);
-// });
-const ogImage = computed(() => {
-    return skinHeadImage.value || '/img/mineskin-social-card.jpg';
-});
-
-useSeoMeta({
-    title: skinNameDisplay,
-    ogTitle: skinNameDisplay,
-    ogImage: ogImage
-})
-
-useHead({
-    link: [{
-        rel: 'canonical',
-        href: `https://beta.mineskin.org/${ skinId.value }`
-    }],
-    script: [{
-        type: 'application/ld+json',
-        innerHTML: JSON.stringify({
-            "@context": "https://schema.org",
-            "@type": "WebPage",
-            "name": skinNameDisplay,
-            "url": `https://beta.mineskin.org/${ skinId.value }`,
-            "image": ogImage.value,
-            "datePublished": new Date(skin.value?.generator?.timestamp || 0).toISOString(),
-        })
-    }]
-})
-
 onMounted(() => {
-    if (skinId.value) {
-        $mineskin.skins.trackView(skinId.value);
-    }
+    router.push(`/skins/${ skinId.value }`);
 })
 
 </script>
