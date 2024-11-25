3a6637e30fa42d4aa3819933ee8831b71d5a7281
Haylee Schäfer <haylee@inventivetalent.org>
2024-11-25 18:18:06 +0100

    ClientOnly

--- components/CreditsInfo.vue
+++ components/CreditsInfo.vue
@@ -1,24 +1,26 @@
 <template>
-    <div v-if="authed">
-        <v-progress-linear
-            v-if="creditsStatus === 'pending'"
-            height="20"
-            indeterminate
-            color="primary"
-        />
-        <v-progress-linear
-            v-else-if="credits"
-            height="20"
-            :model-value="credits.all.total - credits.all.balance"
-            :max="credits.all.total"
-            color="success"
-        >
-            <template v-slot:default>
-                {{ credits.all.total - credits.all.balance }} / {{ credits.all.total }} credits used
-            </template>
-        </v-progress-linear>
-        <a href="https://account.mineskin.org/membership" target="_blank" class="float-end">Details</a>
-    </div>
+    <ClientOnly>
+        <div v-if="authed">
+            <v-progress-linear
+                v-if="creditsStatus === 'pending'"
+                height="20"
+                indeterminate
+                color="primary"
+            />
+            <v-progress-linear
+                v-else-if="credits"
+                height="20"
+                :model-value="credits.all.total - credits.all.balance"
+                :max="credits.all.total"
+                color="success"
+            >
+                <template v-slot:default>
+                    {{ credits.all.total - credits.all.balance }} / {{ credits.all.total }} credits used
+                </template>
+            </v-progress-linear>
+            <a href="https://account.mineskin.org/membership" target="_blank" class="float-end">Details</a>
+        </div>
+    </ClientOnly>
 </template>
 <script setup lang="ts">
 
@@ -37,7 +39,7 @@ const {
     status: creditsStatus,
     refresh: refreshCredits
 } = useLazyAsyncData<BasicCreditInfo>("credits", async () => {
-     if (!authed.value) {
+    if (!authed.value) {
         return {all: {balance: 0, total: 0}};
     }
     return (await $mineskin.me.credits())?.credit;
