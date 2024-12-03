fee254021fd8f331bdd54ecd3e7e200e5882ea71
Haylee Schäfer <haylee@inventivetalent.org>
2024-12-03 11:08:51 +0100

    try prefix_and_default

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -51,7 +51,7 @@ export default defineNuxtConfig({
     },
 
     i18n: {
-        strategy: 'prefix_except_default',
+        strategy: 'prefix_and_default',
         lazy: true,
         locales: [
             {code: 'en', name: 'English', file: 'en.json'},
