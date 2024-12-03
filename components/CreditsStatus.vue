c292d6f032c7a754dbd8b71f641aca833e3b3659
Haylee Schäfer <haylee@inventivetalent.org>
2024-12-03 10:59:03 +0100

    move back down

--- components/CreditsStatus.vue
+++ components/CreditsStatus.vue
@@ -1,21 +1,20 @@
 <template>
     <ClientOnly>
         <div v-if="showCreditsInfo && !generating">
-             <span>{{
-                     $t("This request will consume {credits} if the {skins} successfully generated.", {
-                         credits: $t('n_credits', imageCount || 1),
-                         skins: $t('skin_is_are', imageCount || 1)
-                     })
-                 }}</span>
-            <span>{{
-                    $t("You have {credits} remaining.", {
-                        credits: $t('n_credits', credits?.all?.balance)
-                    })
-                }}</span>
             <Dbg :data="{credits,creditsStatus,authed:authStore.authed}"></Dbg>
             <div v-if="credits">
                 <div v-if="credits?.all?.balance>0">
-                    <span>You have {{ credits?.all?.balance }} credits remaining.</span>
+                       <span>{{
+                               $t("This request will consume {credits} if the {skins} successfully generated.", {
+                                   credits: $t('n_credits', imageCount || 1),
+                                   skins: $t('skin_is_are', imageCount || 1)
+                               })
+                           }}</span>
+                    <span>{{
+                            $t("You have {credits} remaining.", {
+                                credits: $t('n_credits', credits?.all?.balance)
+                            })
+                        }}</span>
                 </div>
                 <div v-else>
                     <span>{{ $t("You do not have any credits remaining.") }}</span><br/>
