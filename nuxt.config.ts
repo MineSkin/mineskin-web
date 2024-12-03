32f8ca695ec1824f1f074bf044f43a89ad519d39
Haylee Schäfer <haylee@inventivetalent.org>
2024-12-03 11:09:59 +0100

    en needs to be last?

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -55,7 +55,6 @@ export default defineNuxtConfig({
         defaultLocale: 'en',
         lazy: true,
         locales: [
-            {code: 'en', name: 'English', file: 'en.json'},
             {code: 'de', name: 'German', file: 'de.json'},
             {code: 'es', name: 'Spanish', file: 'es.json'},
             {code: 'fr', name: 'French', file: 'fr.json'},
@@ -64,6 +63,7 @@ export default defineNuxtConfig({
             {code: 'hi', name: 'Hindi', file: 'hi.json'},
             {code: 'pt', name: 'Portuguese', file: 'pt.json'},
             {code: 'id', name: 'Indonesian', file: 'id.json'},
+            {code: 'en', name: 'English', file: 'en.json'},
         ]
     },
 
