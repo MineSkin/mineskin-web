f131e5ecf2eab07bbfd4ced56dfd0c97c8b56d2e
Haylee Schäfer <haylee@inventivetalent.org>
2024-12-05 13:55:28 +0100

    set tracePropagationTargets

--- plugins/sentry.ts
+++ plugins/sentry.ts
@@ -17,7 +17,7 @@ export default defineNuxtPlugin(nuxtApp => {
                     // Tracing
                     tracesSampleRate: config.public.isDev ? 1.0 : 0.5,
                     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
-                    tracePropagationTargets: ["localhost", /^https:\/\/yourserver\.io\/api/],
+                    tracePropagationTargets: ["localhost", "https://api.mineskin.org", "https://account-api.mineskin.org"],
                     // Session Replay
                     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
                     replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
