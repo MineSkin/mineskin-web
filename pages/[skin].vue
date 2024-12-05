5cc69cc5318c741cf29231e2bfe4e5d4e42cf577
Haylee Schäfer <haylee@inventivetalent.org>
2024-12-05 12:52:32 +0100

    add skin route guard

--- pages/[skin].vue
+++ pages/[skin].vue
@@ -7,8 +7,26 @@
 </template>
 <script setup lang="ts">
 
+import { onBeforeRouteUpdate, useRoute } from "#app";
+import { definePageMeta } from "#imports";
+
+definePageMeta({
+    middleware: [
+        function (to, from) {
+            const {skin} = to.params;
+            if (!skin) {
+                return localePath(`/skins`);
+            }
+            if (skin.length !== 32 && skin.length !== 36 && skin.length !== 8) {
+                return false;
+            }
+        }
+    ]
+})
+
 const localePath = useLocalePath()
 const router = useRouter();
+const route = useRoute();
 
 const skinId = computed<string>(() => {
     const currentRoute = router.currentRoute.value;
