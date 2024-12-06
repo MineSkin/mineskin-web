5182abaca91b951903107195ac7c6207867a48dc
Haylee Schäfer <haylee@inventivetalent.org>
2024-12-06 10:02:36 +0100

    add /account redirect

---
+++ pages/account.vue
@@ -0,0 +1,14 @@
+<style>
+</style>
+<template>
+    <v-container class="mb-4">
+        Redirecting...
+    </v-container>
+</template>
+<script setup lang="ts">
+
+onMounted(() => {
+    window.open('https://account.mineskin.org', '_self');
+})
+
+</script>
