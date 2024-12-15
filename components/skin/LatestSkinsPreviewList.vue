4165e22849c32fc826e0f6fbb58ea2315d8cbdc2
Haylee Schäfer <haylee@inventivetalent.org>
2024-12-15 11:05:29 +0100

    load

--- components/skin/LatestSkinsPreviewList.vue
+++ components/skin/LatestSkinsPreviewList.vue
@@ -1,5 +1,5 @@
 <style scoped>
-.skin-single-row{
+.skin-single-row {
     flex-wrap: nowrap;
     overflow: hidden;
 }
@@ -10,13 +10,14 @@
             <span class="d-inline-block pt-2">{{ $t("Latest Skins") }}</span>
             <v-btn icon="mdi-arrow-right" variant="text" :to="localePath('/skins')"></v-btn>
         </h3>
-        <v-row  class="skin-single-row">
+        <v-row class="skin-single-row">
             <v-slide-group>
-                <skin-link-img class="ma-2" v-show="isHydrated && latestSkins" v-for="skin in latestSkins" :key="skin.uuid" :skin="skin"/>
+                <skin-link-img class="ma-2" v-show="isHydrated && latestSkins" v-for="skin in latestSkins"
+                               :key="skin.uuid" :skin="skin"/>
                 <skin-link-img class="ma-2" v-show="!isHydrated || !latestSkins" v-for="n in 16" :key="n"/>
             </v-slide-group>
         </v-row>
-<!--        <dbg :data="latestSkins"/>-->
+        <!--        <dbg :data="latestSkins"/>-->
     </v-sheet>
 </template>
 <script setup lang="ts">
@@ -34,12 +35,13 @@ const {
     status: latestSkinsStatus,
     refresh: refreshLatestSkins
 } = useLazyAsyncData(async () => {
-    return (await $mineskin.skins.list())?.skins?.slice(0,16)||[];
-},{
+    return (await $mineskin.skins.list())?.skins?.slice(0, 16) || [];
+}, {
     immediate: false
 });
 
 onMounted(() => {
     isHydrated.value = true;
+    refreshLatestSkins();
 })
 </script>
