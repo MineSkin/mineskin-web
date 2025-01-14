aa32d83e8494ff01b4ad4cdbf85c21fdf394b022
Haylee Schäfer <haylee@inventivetalent.org>
2025-01-14 11:51:19 +0100

    add page for random

---
+++ pages/skins/random.vue
@@ -0,0 +1,8 @@
+<template>
+    <v-container>
+        <SkinGallery mode="random"/>
+    </v-container>
+</template>
+<script setup lang="ts">
+import SkinGallery from "~/components/skin/SkinGallery.vue";
+</script>
