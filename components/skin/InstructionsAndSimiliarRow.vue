b5e5da060bbb24abae147ca07d83a3db9002dcba
Haylee Schäfer <haylee@inventivetalent.org>
2025-02-26 12:59:50 +0100

    flag

--- components/skin/InstructionsAndSimiliarRow.vue
+++ components/skin/InstructionsAndSimiliarRow.vue
@@ -3,7 +3,7 @@
         <v-col cols="12" :md="hasSimilar?6:12">
             <SkinInstructionsCard :skin="skin"/>
         </v-col>
-        <v-col cols="12" md="6" v-show="hasSimilar">
+        <v-col cols="12" md="6" v-if="showSimilar" v-show="hasSimilar">
             <SimilarSkinsCard :skin="skin" v-model="hasSimilar"/>
         </v-col>
     </v-row>
@@ -12,6 +12,10 @@
 import type { SkinInfo2 } from "@mineskin/types";
 import SimilarSkinsCard from "~/components/skin/SimilarSkinsCard.vue";
 import SkinInstructionsCard from "~/components/skin/SkinInstructionsCard.vue";
+import { computed } from "vue";
+import { useNuxtApp } from "nuxt/app";
+
+const {$flags} = useNuxtApp();
 
 const props = defineProps<{
     skin: SkinInfo2;
@@ -19,4 +23,5 @@ const props = defineProps<{
 
 const hasSimilar = ref<boolean>(false);
 
+const showSimilar = computed(() => $flags.hasFeature('web.show_similar'));
 </script>
