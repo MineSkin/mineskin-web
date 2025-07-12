a1b983608c751a5ff8b52d218fca14687f4393bb
Haylee Schäfer <haylee@inventivetalent.org>
2025-07-12 10:29:57 +0200

    reduce samples again

--- plugins/sentry.ts
+++ plugins/sentry.ts
@@ -19,12 +19,12 @@ export default defineNuxtPlugin(nuxtApp => {
                         }),
                     ],
                     // Tracing
-                    tracesSampleRate: config.public.isDev ? 1.0 : 0.5,
+                    tracesSampleRate: config.public.isDev ? 1.0 : 0.2,
                     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
                     tracePropagationTargets: ["localhost", "https://api.mineskin.org", "https://account-api.mineskin.org"],
                     // Session Replay
                     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-                    replaysOnErrorSampleRate: 0.5, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+                    replaysOnErrorSampleRate: 0.2, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
                 });
             }
         }
