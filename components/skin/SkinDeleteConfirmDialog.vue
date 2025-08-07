58fdc50e35e2f718e3e61588739092cf3fa3fbba
Haylee Schäfer <haylee@inventivetalent.org>
2025-08-07 14:05:20 +0200

    fix that

--- components/skin/SkinDeleteConfirmDialog.vue
+++ components/skin/SkinDeleteConfirmDialog.vue
@@ -1,53 +1,58 @@
 <template>
     <v-dialog v-model="dialog">
-        <v-card title="Delete Skin?">
-            <template v-slot:activator="{ props: activatorProps }">
-                <v-btn
-                    color="error"
-                    variant="text"
-                    density="compact"
-                    :disabled="!props.canDelete || !!deleteFail"
-                    prepend-icon="mdi-delete"
-                >
-                    Delete Skin
-                </v-btn>
-            </template>
+        <template v-slot:activator="{ props: activatorProps }">
+            <v-btn
+                color="error"
+                variant="text"
+                density="compact"
+                :disabled="!props.canDelete || !!deleteFail"
+                prepend-icon="mdi-delete"
+            >
+                Delete Skin
+            </v-btn>
+        </template>
 
-            <v-card-text>
-                <v-row>
-                    <v-col>
-                        <div>
-                            Are you sure you want to delete this skin? This action is irreversible and will remove the
-                            skin from your account permanently.
-                            <br/>
-                            Alternatively, you can set the visibility to "Private" or "Unlisted" to hide the skin from
-                            public view without deleting it.
-                        </div>
-                        <div>
-                            Please type <strong>delete</strong> in the field below to confirm deletion.
-                            <v-text-field v-model="confirmText" required></v-text-field>
-                        </div>
-                    </v-col>
-                </v-row>
-            </v-card-text>
+        <template v-slot:default="{ isActive }">
+            <v-card title="Delete Skin?">
 
-            <v-card-actions>
-                <v-btn
-                    text="Confirm Deletion"
-                    variant="text"
-                    :disabled="!confirmed"
-                    @click="actuallyDelete()"
-                ></v-btn>
+                <v-card-text>
+                    <v-row>
+                        <v-col>
+                            <div>
+                                Are you sure you want to delete this skin? This action is irreversible and will remove
+                                the
+                                skin from your account permanently.
+                                <br/>
+                                Alternatively, you can set the visibility to "Private" or "Unlisted" to hide the skin
+                                from
+                                public view without deleting it.
+                            </div>
+                            <div>
+                                Please type <strong>delete</strong> in the field below to confirm deletion.
+                                <v-text-field v-model="confirmText" required></v-text-field>
+                            </div>
+                        </v-col>
+                    </v-row>
+                </v-card-text>
 
-                <v-spacer></v-spacer>
+                <v-card-actions>
+                    <v-btn
+                        text="Confirm Deletion"
+                        variant="text"
+                        :disabled="!confirmed"
+                        @click="actuallyDelete()"
+                    ></v-btn>
 
-                <v-btn
-                    text="Cancel"
-                    variant="text"
-                    @click="dialog = false"
-                ></v-btn>
-            </v-card-actions>
-        </v-card>
+                    <v-spacer></v-spacer>
+
+                    <v-btn
+                        text="Cancel"
+                        variant="text"
+                        @click="dialog = false"
+                    ></v-btn>
+                </v-card-actions>
+            </v-card>
+        </template>
     </v-dialog>
 </template>
 <script setup lang="ts">
