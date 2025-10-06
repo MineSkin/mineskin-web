19e47abc78a0c55b387b3ede0f0961a45bd30954
Haylee Schäfer <haylee@inventivetalent.org>
2025-10-06 08:14:18 +0200

    ignore some errors

--- plugins/sentry.ts
+++ plugins/sentry.ts
@@ -22,6 +22,10 @@ export default defineNuxtPlugin(nuxtApp => {
                     tracesSampleRate: config.public.isDev ? 1.0 : 0.2,
                     // Set 'tracePropagationTargets' to control for which URLs distributed tracing should be enabled
                     tracePropagationTargets: ["localhost", "https://api.mineskin.org", "https://account-api.mineskin.org"],
+                    ignoreErrors: [
+                        "TagError: adsbygoogle",
+                        "TurnstileError"
+                    ],
                     // Session Replay
                     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
                     replaysOnErrorSampleRate: 0.2, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
