b6e4f48d78f99442991572b906d8c5e42151e221
Haylee Schäfer <haylee@inventivetalent.org>
2025-11-07 08:40:52 +0100

    include script

--- pages/cape-ad.vue
+++ pages/cape-ad.vue
@@ -1,3 +1,21 @@
 <template>
-    hi
+    <div>
+        hi
+    </div>
+    <component is="script" async
+               :src="'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=' + clientId"
+               crossorigin="anonymous"></component>
 </template>
+<script setup lang="ts">
+import { useAuthStore } from "~/stores/auth";
+import { storeToRefs } from "pinia";
+
+const runtimeConfig = useRuntimeConfig();
+const isDev = runtimeConfig.public.isDev;
+const clientId = runtimeConfig.public.google.adsense;
+
+const ready = ref(false);
+onMounted(() => {
+    ready.value = true;
+});
+</script>
