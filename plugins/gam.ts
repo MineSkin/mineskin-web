9c27b2e1f6c4b6951f0fdaf74eaccc793014e2e7
Haylee Schäfer <mail@inventivetalent.org>
2026-02-27 10:56:19 +0100

    ffs

--- plugins/gam.ts
+++ plugins/gam.ts
@@ -1,6 +1,6 @@
 export default defineNuxtPlugin(() => {
     addRouteMiddleware('gam', () => {
-        (window as any).googletag(window as any) &&
+        (window as any).googletag &&
         (window as any).googletag.cmd.push(() => {
             (window as any).googletag.pubads().refresh();
         });
