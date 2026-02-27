fe510222759ff5f6d8ebb1a82b20f9598fde83b7
Haylee Schäfer <mail@inventivetalent.org>
2026-02-27 10:59:35 +0100

    remove that

--- plugins/gam.ts
+++
@@ -1,8 +0,0 @@
-export default defineNuxtPlugin(() => {
-    addRouteMiddleware('gam', () => {
-        (window as any).googletag &&
-        (window as any).googletag.cmd.push(() => {
-            (window as any).googletag.pubads().refresh();
-        });
-    }, {global: true});
-})
